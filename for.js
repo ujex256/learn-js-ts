776be90a87e9daa42c143375da0c52f04756f668
fffena <132272963+fffena@users.noreply.github.com>
2023-11-21 13:13:55 +0000

    add for

---
+++ for.js
@@ -0,0 +1,20 @@
+for (let i = 0;i < 5; i++) {
+    console.log(`hello!${i}`)
+}
+
+// Arrayを回す
+const roopString = "あいうえお"
+const roopArr = [...roopString]
+for (const elem of roopArr) {
+    console.log(elem);
+}
+
+function randInt(min, max) {
+    return Math.random() * (max - min) + min;
+}
+// const randInt = (min, max) => { return Math.random() * (max - min) + min }
+
+const randomIntArr = [...Array(10)].map((i) => { return randInt(0, 9999) })
+for (const elem of randomIntArr) {
+    console.log(Math.round(elem).toString())
+}

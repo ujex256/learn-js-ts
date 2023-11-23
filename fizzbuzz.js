a9869b5be0a403d0b8942bd10f60c24b8af6817a
fffena <132272963+fffena@users.noreply.github.com>
2023-11-23 00:43:49 +0000

    feat: fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+// sleep関数はコピペ
+// Promiseを指定時間後にsetTimeoutでresolveするらしい
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
+(async function () {
+    for (let i = 1; i < 21; i++) {
+        if (i % 15 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i.toString())
+        }
+        await sleep(300)
+    }
+}())

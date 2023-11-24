2522299203736145beb7a61f35e02f749b549911
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2023-11-24 01:16:33 +0000

    モジュールにして、top level awaitが使えるように

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,17 @@
 // sleep関数はコピペ
 // Promiseを指定時間後にsetTimeoutでresolveするらしい
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = ms => new Promise((resolve) => setTimeout(resolve, ms));
 
 
-(async function () {
-    for (let i = 1; i < 21; i++) {
-        if (i % 15 === 0) {
-            console.log("FizzBuzz")
-        } else if (i % 3 === 0) {
-            console.log("Fizz")
-        } else if (i % 5 === 0) {
-            console.log("Buzz")
-        } else {
-            console.log(i.toString())
-        }
-        await sleep(300)
+for (let i = 1; i <= 30; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
     }
-}())
+    await sleep(300)
+}
